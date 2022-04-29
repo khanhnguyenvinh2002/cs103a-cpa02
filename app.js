fc6c6cdffa0aecc38b5c134169a9f5f0956691d4
khanhnguyenvinh2002 <giakhanhvy@gmail.com>
2022-04-29 15:52:45 -0400

    using env

--- app.js
+++ app.js
@@ -13,15 +13,19 @@ const debug = require("debug")("personalapp:server");
 const layouts = require("express-ejs-layouts");
 const methodOverride = require('method-override');
 
+const dotenv = require("dotenv");
+dotenv.config();
 // *********************************************************** //
 //  Connecting to the database
 // *********************************************************** //
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true}).then(()=>{
   console.log('connected to mongoDB')
-}).catch(err => console.log(err));;
+}).catch(err => console.log(err));
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () { console.log("we are connected!!!") });
